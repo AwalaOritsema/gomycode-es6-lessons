65ad67062642b669c802743cfca63d42e70d241f
Michael Peter <michaelhpet@gmail.com>
2023-03-31 21:20:06 +0100

    declare variables with let, const, and var

---
+++ variables.js
@@ -0,0 +1,37 @@
+// Declaring variables in JavaScript version 6 (es6/es2015)
+
+// Always use the const keyword to declare variables
+const firstName = "John"
+// firstName = "Dan" // this will cause an error: you should not assign a new value to a constant variable
+
+// ...except if you really need to change the value. In that case, you can use the let keyword
+let lastName = "Doe"
+lastName = "Finn" // This is acceptable because the variable is NOT constant
+// console.log(lastName)
+
+// Before ES6, the var keyword was used
+var otherName = "Alex"
+otherName = "Dan" // This is acceptable because the variable is NOT constant
+// console.log(otherName)
+
+// Difference between let/const and var
+const people = ["Dan", "Joe", "Alex", "John", "Finn"]
+
+for (const person of people) {
+  // This variable exists only inside this for-loop
+  let personGreeting = person + ", hey!"
+  // console.log(personGreeting)
+}
+
+// Trying to use the for-loop variable outside of the loop won't work
+// console.log(personGreeting)
+
+// However, if the variable inside the loop was declared with the var keyword, it would be accessible outside its scope
+for (const person of people) {
+  // This variable exists in its scope (the for-loop) as well as outside the for-loop
+  var personGreeting = person + ", hi!"
+  // console.log(personGreeting)
+}
+
+// Trying to use the for-loop variable outside of the loop will work
+// console.log(personGreeting, "outside")
