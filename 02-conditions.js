000ae27f13433fcde516ef96dba36a7268206d04
Michael Peter <michaelhpet@gmail.com>
2023-04-04 19:31:39 +0100

    revisit ternary operator

--- 02-conditions.js
+++ 02-conditions.js
@@ -1,6 +1,6 @@
 // Conditions (also known as constrol flow) are used to control a program
 
-const myNumber = 4
+const myNumber = 10
 
 // if (myNumber > 5) {
 //   console.log("My number is greater than 5")
@@ -16,9 +16,23 @@ const myNumber = 4
 //   console.log("My number is less or equal to 5")
 // }
 
+// myNumber > 5
+//   ? console.log("My number is greater than 5")
+//   : console.log("My number is less or equal to 5")
+
 // ternary operator
 // condition (if) ? code (expression) : fallback (otherwise/else)
 
+// const value = myNumber > 5 ? "A" : "B"
+
+// let value
+
+// if (myNumber > 5) {
+//   value = "A"
+// } else {
+//   value = "B"
+// }
+
 const value = myNumber > 5 ? "A" : "B"
 
-console.log(value)
+// console.log(value)
